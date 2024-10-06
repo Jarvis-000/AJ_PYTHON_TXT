71f31cb7efd93dac6a79d7ff32eb1f4577f1a0d7
Jarvis-000 <letshelpofficial2024@gmail.com>
2024-10-06 22:00:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
+import logging
 
-API_ID = API_ID =  23981002
+API_ID = 23981002
 
 API_HASH = os.environ.get("API_HASH", "9549825af7363be26b4b53e5bb3e737a")
 
@@ -8,9 +9,15 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "7521089253:AAGHqiw4e7ewdc1qt7eonChimNf-
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", "6348445990" ))
+OWNER = int(os.environ.get("OWNER", "6348445990"))
 
-LOG = ,
+# Setting up logging
+LOG = logging.getLogger("my_logger")
+LOG.setLevel(logging.INFO)
+handler = logging.StreamHandler()  # Outputs logs to console
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+LOG.addHandler(handler)
 
 # UPDATE_GRP = , # bot sat group
 
@@ -21,7 +28,5 @@ try:
     for x in (os.environ.get("ADMINS", "6348445990").split()):
         ADMINS.append(int(x))
 except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
+    raise Exception("Your Admins list does not contain valid integers.")
 ADMINS.append(OWNER)
-
-
